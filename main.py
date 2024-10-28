239ee3942182820cf3e63dac39924c90b332609d
higgsbb <ben.higgs@hotmail.co.nz>
2024-10-28 21:15:18 +1300

    more edits
    more edits

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ import matplotlib.colors as mcolors
 import json
 
 
-
 colour_scales = {"viridis": plt.cm.viridis}
-
-
 cmap = plt.cm.viridis
 
 st.set_page_config(layout="wide")
@@ -27,14 +24,18 @@ for i in data_info.index:
     field_lookup[
         f"{data_info["Description"][i]}, {data_info["Sex"][i]}, {data_info["Year"][i]}"
         ] = data_info["Field_Name"][i]
-    
+
+st.title("Essex MSOA Statistics")
+
 col_name_selection = st.selectbox(
-    "Select a criteria to set colour by",
-    (f for f in field_lookup),
+    label = "Select a criteria to set colour by",
+    options = (f for f in field_lookup),
+    key = 4564346573746576
 )
 height_name_selection = st.selectbox(
-    "Select a criteria to set elevation by",
-    (f for f in field_lookup),
+    label = "Select a criteria to set elevation by",
+    options = (f for f in field_lookup),
+    key = "height_name_selection"
 )
 
 col_selection = field_lookup[col_name_selection]
@@ -54,36 +55,30 @@ data['height'] = data["height_selection_norm"]*max_height
 # Scale RGB values to 0-255 range
 data['colour'] = data['colour'].apply(lambda rgb: [int(255 * v) for v in rgb])
 
-#make polygon layers for all area codes
-
-#########
-import requests
-#from io import StringIO
-
-##########
-
-def get_area_code_data(area_code):
+def get_area_code_data(area_code, data, stat_cols):
     print(area_code)
 
     with open(rf'area_code_geojson/{area_code}.geojson') as f:
         geodata = json.load(f)
-    # response = requests.get(f"https://findthatpostcode.uk/areas/{area_code}.geojson")
-    # geodata = response.json()
-    # response.close()
 
-    # with open(rf'area_code_geojson/{area_code}.geojson', 'w') as f:
-    #     json.dump(geodata, f)
-    
+    # append statistics to geojson
+    for s in stat_cols:
+        stat = round(data.loc[
+            data["Area Code"] == area_code, s].max(),
+            2)
+        geodata['features'][0][s] = stat
     return geodata
 
 
-def get_geo_data(data):
-    geo_data = data["Area Code"].apply(lambda x: get_area_code_data(x))
+def get_geo_data(data, stat_cols):
+    geo_data = data["Area Code"].apply(lambda x: get_area_code_data(
+        x, data, stat_cols)
+        )
+
     return geo_data
 
-
 layers = []
-geo_data = get_geo_data(data = data)
+geo_data = get_geo_data(data = data, stat_cols = [field_lookup[f] for f in field_lookup])
 for i in data.index:
     #areacode = data["Area Code"][i]
    
@@ -104,6 +99,10 @@ for i in data.index:
     )
     layers.append(layer_i)
 
+tooltip_label = "<b>{name}</b><br/><br/>"
+for s in [field_lookup[col_name_selection], field_lookup[height_name_selection]]:
+    tooltip_label = f"{tooltip_label}<b>{s}: </b>{{{s}}}<br/>"
+
 map = st.pydeck_chart(
     pydeck_obj = pdk.Deck(
         map_style=None,
@@ -114,7 +113,7 @@ map = st.pydeck_chart(
             pitch=50,
         ),
         layers=layers,
-        tooltip = {"html": "<b>Elevation Value:</b> {name} <br/> <b>Color Value:</b> {name}",
+        tooltip = {"html": tooltip_label,
                         "style": {
                                 "backgroundColor": "steelblue",
                                 "color": "white"
@@ -123,11 +122,11 @@ map = st.pydeck_chart(
     )
 )
 
-# Add a color scale legend
+#Add a color scale legend
 colours = ",".join([mcolors.to_hex(cmap(i / 10)) for i in range(11)])
-min_val=min(data[col_selection])
-mid_val=(min(data[col_selection]) + max(data[col_selection])) / 2
-max_val=max(data[col_selection])
+min_val = round(min(data[col_selection]), 2)
+mid_val = round((min(data[col_selection]) + max(data[col_selection])) / 2, 2)
+max_val = round(max(data[col_selection]), 2)
 color_scale_html = f"""
     <div style="text-align: center">{col_name_selection}</div>
     <div style="position: relative; top: 0px; height: 30px; width: 100%;">
@@ -141,6 +140,7 @@ color_scale_html = f"""
 #st.text(str(col_name_selection))
 st.markdown(color_scale_html, unsafe_allow_html=True)
 
-
-# # x = 1
-
+st.write('')
+st.write('')
+st.write('MSOA polgons obtained from https://findthatpostcode.uk/')
+st.write('Source data supplied by Essex County Council')
